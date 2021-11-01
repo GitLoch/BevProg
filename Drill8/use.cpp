46e139322375ea603a816f9a093a539fcce591dc
GitLoch <pastilajos86@gmail.com>
2021-11-01 15:05:32 +0100

    Drill8.cpp-ben a hármas feladat, az 1. és 2. feladat a többi 3 fájlban.

--- Drill8/use.cpp
+++ Drill8/use.cpp
@@ -6,25 +6,52 @@
  	foo = 7;
  	print_foo();
  	print(99);
- 	
+
  	
  	int x = 7;
 	int y =9;
+	
 	swap_v(x,y); 		//compiled
+	print(x);
+	print(y);
+	
 	swap_v(7,9); 		//compiled
-	swap_r(x,y);
+	print(x);
+	print(y);
+	
+	swap_r(x,y);		//compiled
+	print(x);
+	print(y);
+	
 	//swap_r(7,9);		//non-const value to type int&
+	
 	const int cx = 7;
 	const int cy = 9;
+	
 	swap_v(cx,cy);		//compiled
+	print(cx);
+	print(cy);
+	
 	swap_v(7.7,9.9);	//compiled
+	print(cx);
+	print(cy);
+	
 	//swap_r(cx,cy);	//binding reference of type 'int&' to 'const int'
 	//swap_r(7.7,9.9);	//non-const value to type int&
 	double dx = 7.7;	
 	double dy = 9.9;
+	
 	swap_v(dx,dy);		//compiled
+	print(dx);
+	print(dy);
+	
 	swap_v(7.7,9.9);	//compiled
+	print(dx);
+	print(dy);
+	
 	//swap_r(dx,dy);	//non-const value to type int&
+	
 	//swap_r(7.7,9.9);	//non-const value to type int&
+	
  }
  
