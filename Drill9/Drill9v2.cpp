d17bd0604ab9c73492737fe014133c8104120110
Lochel <90399936+GitLoch@users.noreply.github.com>
2021-11-15 17:32:05 +0100

    2. Version

---
+++ Drill9/Drill9v2.cpp
@@ -0,0 +1,32 @@
+#include "../std_lib_facilities.h"
+
+struct Date {
+	int y;
+	int m;
+	int d;
+	Date(int year, int month, int day);
+	void add_day(int n);
+};
+
+Date::Date(int year, int month, int day){
+	y=year;
+	m=month;
+	d=day;
+}
+
+void Date::add_day(int n){
+	d += n;
+}
+
+ostream& operator<<(ostream& os, const Date& d){
+	return os << '(' << d.y << ',' << d.m << ',' << d.d << ')';
+}
+
+int main(){
+	Date today {1978,6,25};
+	Date tomorrow = today;
+	tomorrow.add_day(1);
+	cout << today << endl << tomorrow << endl;
+	return 0;
+}
+
